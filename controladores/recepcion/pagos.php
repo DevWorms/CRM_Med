e9c2598ea813e2eb07e791b496124b3823ab42fa
Ricardo Osorio <ricardo.0x7@gmail.com>
2017-08-30 10:15:10 -0500

    Fix input type

--- controladores/recepcion/pagos.php
+++ controladores/recepcion/pagos.php
@@ -60,7 +60,7 @@
 
                                                 <br>
                                                 <label for="fecha">Fecha de Recibo</label>
-                                                <input type="date" id="fecha" name="fecha" class="form-control">
+                                                <input type="text" id="fecha" name="fecha" class="form-control">
 
                                                 <br>
                                                 <div class="form-group" align="right">
