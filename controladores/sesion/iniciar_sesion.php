6533c5c8fa11e9a619815e46321fc33517d72c54
César Cano <cesarcanojmz@gmail.com>
2017-09-13 20:19:18 -0500

    ...

--- controladores/sesion/iniciar_sesion.php
+++ controladores/sesion/iniciar_sesion.php
@@ -17,13 +17,20 @@ if (isset($_POST['id_usuario']) && isset($_POST['contrasena'])) {
         // Valida el inicio de sesión normal
         if (count($resultado) == 1) {
             $resultado = $resultado[0];
-            if ($resultado['citas'] == 1 && $resultado['recepcion'] == 0) {
+            if ($resultado['citas'] == 1 && $resultado['recepcion'] == 0) { //citas
                 $res['url'] = "citas_programar";
-            } elseif ($resultado['citas'] == 0 && $resultado['recepcion'] == 0 && $resultado['farmacia'] == 1) {
-                $res['url'] = "orden_compra";
-            } else {
+            } elseif ($resultado['citas'] == 0 && $resultado['recepcion'] == 0 && $resultado['farmacia'] == 1) { // Si es farmacia
+                $res['url'] = "catalogo";
+            } elseif ( $resultado['citas'] == 0 && $resultado['recepcion'] == 0 && $resultado['farmacia'] == 0 
+                && $resultado['medico'] == 1 ) { // Si es medico
+                $res['url'] = "mis_pacientes";
+            } elseif ($resultado['citas'] == 1 && $resultado['recepcion'] == 1 && $resultado['farmacia'] == 1 
+            && $resultado['medico'] == 1 ) { // Admin
+                $res['url'] = "citas";
+            } else { // Si es medico
                 $res['url'] = "citas";
             }
+
             session_start();
             $_SESSION["Id"] = $resultado['id'];
             $_SESSION["Nombre"] = $resultado['nombre'] . ' ' . $resultado['apPaterno'] . ' ' . $resultado['apMaterno'];
