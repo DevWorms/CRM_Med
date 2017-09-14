46103900346a159364a3413c807b70b0dc73aa6d
César Cano <cesarcanojmz@gmail.com>
2017-09-13 20:16:36 -0500

    Fix

--- controladores/sesion/iniciar_sesion.php
+++ controladores/sesion/iniciar_sesion.php
@@ -1,42 +1,29 @@
 <?php
 require_once dirname(__FILE__) . '/../datos/ConexionBD.php';
-
 if (isset($_POST['id_usuario']) && isset($_POST['contrasena'])) {
     $id_usuario = $_POST['id_usuario'];
     $contrasena = hash('sha256', $_POST['contrasena']);
-
         $pdo = ConexionBD::obtenerInstancia()->obtenerBD();
         $consultaSesion = "SELECT * FROM usuarios INNER JOIN accesos ON usuarios.id = accesos.id_usuario WHERE numeroUsuario = ? AND password = ?";
-
         $sentenciaSesion = $pdo->prepare($consultaSesion);
         $sentenciaSesion->bindParam(1, $id_usuario);
         $sentenciaSesion->bindParam(2, $contrasena);
         $sentenciaSesion->execute();
         $resultado = $sentenciaSesion->fetchAll();
-
         $res = [
             'estado' => 0,
             'mensaje' => ''
         ];
-
         // Valida el inicio de sesión normal
         if (count($resultado) == 1) {
             $resultado = $resultado[0];
-
-            if ($resultado['citas'] == 1 && $resultado['recepcion'] == 0) { //citas
+            if ($resultado['citas'] == 1 && $resultado['recepcion'] == 0) {
                 $res['url'] = "citas_programar";
-            } elseif ($resultado['citas'] == 0 && $resultado['recepcion'] == 0 && $resultado['farmacia'] == 1) { // Si es farmacia
-                $res['url'] = "catalogo";
-            } elseif ( $resultado['citas'] == 0 && $resultado['recepcion'] == 0 && $resultado['farmacia'] == 0 
-                && $resultado['medico'] == 1 ) { // Si es medico
-                $res['url'] = "mis_pacientes";
-            } elseif ($resultado['citas'] == 1 && $resultado['recepcion'] == 1 && $resultado['farmacia'] == 1 
-            && $resultado['medico'] == 1 ) { // Admin
-                $res['url'] = "1";
-            } else { // Si es medico
+            } elseif ($resultado['citas'] == 0 && $resultado['recepcion'] == 0 && $resultado['farmacia'] == 1) {
+                $res['url'] = "orden_compra";
+            } else {
                 $res['url'] = "citas";
             }
-
             session_start();
             $_SESSION["Id"] = $resultado['id'];
             $_SESSION["Nombre"] = $resultado['nombre'] . ' ' . $resultado['apPaterno'] . ' ' . $resultado['apMaterno'];
@@ -49,7 +36,6 @@ if (isset($_POST['id_usuario']) && isset($_POST['contrasena'])) {
         } else {
             $res['mensaje'] = "Usuario o contraseña incorrectos";
         }
-
         // Devuelve json como respuesta
         echo json_encode($res);
 } else {
