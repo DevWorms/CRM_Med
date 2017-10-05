403c701521b6ca4715b616afda05f692f50eb1d4
Ricardo Suárez <ur.suarez@gmail.com>
2017-10-04 23:45:46 -0500

    Financiero

---
+++ controladores/admin/finanzas-caja.php
@@ -0,0 +1,35 @@
+<?php
+    include dirname(__FILE__) . '/../layouts/header.php';
+?>
+
+<div class="container">
+    <?php include dirname(__FILE__) . "/../layouts/navbar.php"; ?>
+</div>
+
+<!-- Page Content -->
+<div class="container">
+    <div class="row">
+        
+        <?php include dirname(__FILE__) . "/../layouts/menu.php"; ?>
+
+        <div class="col-md-9">
+            <div class="thumbnail">
+                <div class="caption-full">
+                    <h3 style="display:inline; color:#337ab7;">Financiero - Corte de Caja, Entradas y Salidas de Dinero </h3>
+                    <span class=" glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title=""></span>
+                    <div id="error"></div>
+                    <hr>
+                    <br>
+                    
+                    <div class="row">
+                        <div class="col-md-12">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include dirname(__FILE__) . '/../layouts/footer.php'; ?>
