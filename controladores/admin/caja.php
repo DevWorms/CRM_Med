a11745c27b3fd9797d010fd8bd7ddd0260bb264c
MartinGarCar <martin_garcia.775@hotmail.com>
2017-10-05 20:40:33 -0500

    Administrador - Corte de Caja

--- controladores/admin/caja.php
+++ controladores/admin/caja.php
@@ -6,7 +6,91 @@
     <?php include dirname(__FILE__) . "/../layouts/navbar.php"; ?>
 </div>
 
+<link href='../../css/calendarioCorte/fullcalendar.min.css' rel='stylesheet' />
+<script src='../../js/calendarioCorte/lib/moment.min.js'></script>
+<script src='../../js/calendarioCorte/lib/jquery.min.js'></script>
+<script src='../../js/calendarioCorte/fullcalendar.min.js'></script>
+<script src='../../js/calendarioCorte/locale-all.js'></script>
+
 <!-- Page Content -->
+
+<script>
+
+    $(document).ready(function() {
+        
+        $('#calendarCorte').fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,basicWeek,basicDay'
+            },
+            locale: 'es',
+            defaultDate: '2017-09-12',
+            navLinks: true, // can click day/week names to navigate views
+            editable: true,
+            eventLimit: true, // allow "more" link when too many events
+            events: [
+                {
+                    title: 'All Day Event',
+                    start: '2017-09-01'
+                },
+                {
+                    title: 'Long Event',
+                    start: '2017-09-07',
+                    end: '2017-09-10'
+                },
+                {
+                    id: 999,
+                    title: 'Repeating Event',
+                    start: '2017-09-09T16:00:00'
+                },
+                {
+                    id: 999,
+                    title: 'Repeating Event',
+                    start: '2017-09-16T16:00:00'
+                },
+                {
+                    title: 'Conference',
+                    start: '2017-09-11',
+                    end: '2017-09-11'
+                },
+                {
+                    title: 'Meeting',
+                    start: '2017-09-12T10:30:00',
+                    end: '2017-09-12T12:30:00'
+                },
+                {
+                    title: 'Lunch',
+                    start: '2017-09-12T12:00:00'
+                },
+                {
+                    title: 'Meeting',
+                    start: '2017-09-12T14:30:00'
+                },
+                {
+                    title: 'Happy Hour',
+                    start: '2017-09-12T17:30:00'
+                },
+                {
+                    title: 'Dinner',
+                    start: '2017-09-12T20:00:00'
+                },
+                {
+                    title: 'Birthday Party',
+                    start: '2017-09-13T07:00:00'
+                },
+                {
+                    title: 'Corte de Caja',  
+                                    
+                    start: '2017-09-28'
+                }
+            ]
+        });
+        
+    });
+
+</script>
+
 <div class="container">
     <div class="row">
         
@@ -15,12 +99,33 @@
         <div class="col-md-9">
             <div class="thumbnail">
                 <div class="caption-full">
-                    <h2 style="display:inline; color:#337ab7;">Administrador - Corte de Caja </h2>
-                    <span class=" glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title="Genera por cada día un corte de caja que incluye ingresos y egresos."></span>
+                    <h3 style="display:inline; color:#337ab7;">Administrador - Corte de Caja</h3>
+                    <span class=" glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title=""></span>
                     <div id="error"></div>
+                    <hr>
                     <br>
+                    
                     <div class="row">
                         <div class="col-md-12">
+                            <div id='calendarCorte'></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">    
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>          
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-6">
                             <table class="table table-bordered">
                                 <thead>
                                     <tr>
