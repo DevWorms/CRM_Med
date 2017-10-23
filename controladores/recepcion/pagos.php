f30dbd8144a142cbc18ffb1e287ada7037bc5f03
Richard VelRo <rk_rivero@outlook.com>
2017-10-23 13:54:39 -0500

    Fuck this shit

--- controladores/recepcion/pagos.php
+++ controladores/recepcion/pagos.php
@@ -9,8 +9,41 @@
     <?php include dirname(__FILE__) . "/../layouts/navbar.php"; ?>
 </div>
 <!-- /NAVBAR -->
-    
 
+<!-- TABS LOGIC -->
+ 
+<script type="text/javascript">
+    $(document).ready(function(){
+        //La que inicia
+        $('ul.tabs_pagos li a:first').addClass('activee');
+        $('ul.tabs_pagos li a span:first').addClass('tab-textselec');
+        //Oculta los tabs
+        $('.secciones article').hide();
+        //Muestra el primer tab
+        $('.secciones article:first').show();
+        $('ul.tabs_pagos li a').click(function(){
+            //Remueve todas las clases 'active'
+            $('ul.tabs_pagos li a').removeClass('activee');
+            //Remueve todos los estilos color blanco (letra)
+            $('ul.tabs_pagos li a span').removeClass('tab-textselec');
+            //Agrega color gris a todos (letra)
+            $('ul.tabs_pagos li a span').addClass('tab-text');
+            //Al tab seleccionado le agrega la clase 'active'
+            $(this).addClass('activee');
+            //Oculta las demas secciones
+            $('.secciones article').hide();
+            //almacena en una varaible el valor del href '#tab()'
+            var activeTab = $(this).attr('href'); 
+            //Muestra la etiqueta con ese tab
+            $(activeTab).show();
+            $(activeTab+'n').removeClass('tab-text');
+            $(activeTab+'n').addClass('tab-textselec');
+            return false;
+        });
+    });
+</script>
+<!-- /TABS LOGIC-->
+ 
 <div class="container">
     <div class="row">
         <!--MENU-->
@@ -22,15 +55,141 @@
             <div class="thumbnail">
 
                 <!--TITULO-->
-                <h2 style="display:inline; color:#337ab7;"> &nbsp; Recepción - Pagos</h2>
+                <h2 style="display:inline; color:#337ab7;">Recepción - Pagos</h2>
                 <span class=" glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title="Registra pagos a los pacientes así como ventas en recepción, similar a la versión beta."></span>
                 <hr>
                 <!-- / TITULO -->
                 
             
                 <div class="wrap">
+                    <!--TABS MENU-->
+ 
+                    <ul class="tabs_pagos">
+                        <li>
+                            <a href="#tab1" style="text-decoration:none;">
+                                <span class="tab-text" id="tab1n">
+                                    Pagos Versión 1
+                                </span>
+                            </a>
+                        </li>
+ 
+                        <li>
+                            <a href="#tab2" style="text-decoration:none;">
+                                <span class="tab-text" id="tab2n">
+                                    Pagos Versión 2
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+ 
+                    <!-- / TABS MENU -->
+ 
+                    <hr>
+ 
                     <!--TABS CONTENT-->
                     <div class="secciones">
+
+                        <!-- TAB 1 PAGO VERSION 1 -->
+                        <article id="tab1">
+                            <!--BUSCADOR-->
+                            <div class="form-group row">
+                                <div class="col-md-12" align="center">
+                                    <div id="custom-search-input">
+                                        <div class="input-group col-md-9" >
+                                            <input type="text" class="search-query form-control" id="param" name="param" placeholder="Buscar Paciente..." />
+                                            <span class="input-group-btn">
+                                                <button class="btn btn-primary" type="button" id="busqueda">
+                                                    <span class=" glyphicon glyphicon-search"></span>
+                                                </button>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="error"></div>
+                            <input type="hidden" name="user_id" name="user_id">
+                            <!--/ BUSCADOR --> 
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <!--  FORMULARIO PAGO V1 NUEVO PAGO-->
+                                    <form id="nuevoPago" name="nuevoPago" method="POST">
+                                        <h2>Nuevo Pago</h2>
+                                        <div class="jumbotron">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <label for="selec_presup">Selecciona Presupuesto</label>
+                                                    <select id="selec_presup" class="form-control" name="selec_presup">
+                                                        <option value="0">Otros</option>
+                                                    </select>
+                                                    <br>
+                                                    <label for="monto">Monto</label>
+                                                    <input type="number" id="monto" name="monto" class="form-control" required>
+ 
+                                                    <br>
+                                                    <label for="concepto">Concepto</label>
+                                                    <input type="text" id="concepto" name="concepto" class="form-control" required>
+ 
+                                                    <br>
+                                                    <label for="recibo">Número de Recibo</label>
+                                                    <input type="number" id="recibo" name="recibo" class="form-control" required>
+ 
+                                                    <br>
+                                                    <label for="fecha">Fecha de Recibo</label>
+                                                    <input type="text" id="fechaAbierta" name="fecha" class="form-control" readonly>
+
+                                                    <br>
+                                                    <div class="form-group" align="right">
+                                                        <button type="submit" class="btn btn-primary" id="registrar">
+                                                            Registrar Pago
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <!--  /FORMULARIO PAGO V1 NUEVO PAGO-->
+                                </div>
+                                <div class="col-md-6">
+                                    <!-- FORMULARIO PAGO V1 MUEVO PRESUPUESTO-->
+                                    <form id="nuevoPresupuesto" name="nuevoPresupuesto" method="POST">
+                                        <h2>Nuevo Presupuesto</h2>
+                                        <div class="jumbotron">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <label for="nom_presup">Nombre Presupuesto</label>
+                                                    <input type="text" id="nombre" name="nombre" class="form-control" required>
+
+                                                    <br>
+                                                    <label for="costo_pres">Costo Presupuesto</label>
+                                                    <input type="number" id="precio" name="precio" class="form-control" required>
+ 
+                                                    <br>
+                                                    <div class="form-group" align="right">
+                                                        <button type="submit" class="btn btn-primary" id="registrar">Crear nuevo presupuesto</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <!-- /FORMULARIO PAGO V1 NUEVO PRESUPUESTO-->
+                                    <!--RESUPUESTO DEL PACIENTE -->
+                                    <h2>Presupuestos del Paciente</h2>
+                                    <div class="jumbotron">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <select id="mostrar_presup" class="form-control" name="mostrar_presup">
+                                                    <option value="0">Otros</option>
+                                                 </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- /RESUPUESTO DEL PACIENTE -->
+                                </div>
+                            </div>     
+ 
+                        </article>
+                        <!-- /TAB 1 PAGO VERSION 1 -->
+
                         <!-- TAB 2 PAGO VERSION 2-->
                         <article id="tab2">
                             <div class="jumbotron">
